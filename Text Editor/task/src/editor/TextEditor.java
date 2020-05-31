a0c541ccef83a2e88500abd572bd718cbff7b667
Daria <dasha_smirn@mail.ru>
2020-05-31 22:37:28 +0300

    third stage passed

--- Text Editor/task/src/editor/TextEditor.java
+++ Text Editor/task/src/editor/TextEditor.java
@@ -1,6 +1,7 @@
 package editor;
 
 import javax.swing.*;
+import java.awt.event.KeyEvent;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -8,19 +9,22 @@ import java.awt.*;
 import java.nio.file.Paths;
 
 public class TextEditor extends JFrame {
+    JTextArea textEditArea;
+    JTextField fileNameField;
+
     public TextEditor() {
         super("The second stage");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(440, 460);
         setLocationRelativeTo(null);
-        setVisible(true);
         setLayout(null);
         InitComponents();
-
+        InitMenu();
+        setVisible(true);
     }
 
     private void InitComponents() {
-        JTextArea textEditArea = new JTextArea();
+        textEditArea = new JTextArea();
         textEditArea.setName("TextArea");
         JScrollPane scrollableTextArea = new JScrollPane(textEditArea);
         scrollableTextArea.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
@@ -29,7 +33,7 @@ public class TextEditor extends JFrame {
         scrollableTextArea.setName("ScrollPane");
         add(scrollableTextArea);
 
-        JTextField fileNameField = new JTextField();
+        fileNameField = new JTextField();
         fileNameField.setName("FilenameField");
         fileNameField.setBounds(20, 10, 200, 20);
         add(fileNameField);
@@ -46,25 +50,54 @@ public class TextEditor extends JFrame {
         loadButton.setName("LoadButton");
         loadButton.setBounds(310, 10, 90, 20);
         loadButton.addActionListener(actionEvent -> {
-            String contained;
-            textEditArea.setText("");
-            try {
-                contained = LoadFile(fileNameField.getText());
-                textEditArea.setText(contained);
-            } catch (IOException e) {
-            }
+            LoadFile(fileNameField.getText());
         });
         add(loadButton);
     }
 
-    public void SaveFile(String fileName, String contained) {
+    private void InitMenu() {
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        fileMenu.setMnemonic(KeyEvent.VK_F);
+        fileMenu.setName("MenuFile");
+
+        JMenuItem saveMenuItem = new JMenuItem("Save");
+        saveMenuItem.setName("MenuSave");
+        saveMenuItem.addActionListener(event -> SaveFile(fileNameField.getText(), textEditArea.getText()));
+
+        JMenuItem loadMenuItem = new JMenuItem("Load");
+        loadMenuItem.setName("MenuLoad");
+        loadMenuItem.addActionListener(event -> LoadFile(fileNameField.getText()));
+
+        JMenuItem exitMenuItem = new JMenuItem("Exit");
+        exitMenuItem.setName("MenuExit");
+        exitMenuItem.addActionListener(event -> {
+            this.dispose();
+            System.exit(0);
+        });
+
+        fileMenu.add(saveMenuItem);
+        fileMenu.add(loadMenuItem);
+        fileMenu.addSeparator();
+        fileMenu.add(exitMenuItem);
+
+        menuBar.add(fileMenu);
+
+        setJMenuBar(menuBar);
+    }
+    private void SaveFile(String fileName, String contained) {
         try (FileWriter writer = new FileWriter(fileName, false)) {
             writer.write(contained);
         } catch (IOException e) {
         }
     }
 
-    public static String LoadFile(String fileName) throws IOException {
-        return new String(Files.readAllBytes(Paths.get(fileName)));
+    private void LoadFile(String fileName) {
+        try {
+            String contained = new String(Files.readAllBytes(Paths.get(fileName)));
+            textEditArea.setText(contained);
+        } catch (IOException e) {
+            textEditArea.setText("");
+        }
     }
 }
